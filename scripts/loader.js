514ed0cd5bb5e6b0e65f39ce0c4efec85a7d0301
ANT <68531196+ant-7802@users.noreply.github.com>
2022-01-04 18:40:09 +0000

    best update yet

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,10 +1,8 @@
 const deepPath = document.getElementsByClassName("navbar")[0].classList.contains("deeppath");
 const very = document.getElementsByClassName("navbar")[0].classList.contains("very");
 
-document.getElementsByClassName("navbar")[0].innerHTML = `   <script src="/app.js"></script>
+document.getElementsByClassName("navbar")[0].innerHTML = `
       <a href="/" class="button">Home</a>
-      <a href="/proxy" class="button">Proxy</a>
-      <a href="/games" class="button">Games</a>
-      <a href="/help" class="button">Help</a>
-      <a href="/unblock" class="button">Unblocked Websites</a>
-      <a href="/chatbox" class="button">Chatbox</a>`;
+      <a href="/p" class="button">Proxy</a>
+      <a href="/more" class="button">Games</a>
+      <a href="/help" class="button">Help</a>`;
