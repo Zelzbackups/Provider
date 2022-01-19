16cce72cd6a772e8c87fd5505626c27252fb8015
ANT <68531196+ant-7802@users.noreply.github.com>
2022-01-19 18:59:19 +0000

    proxy fix maybe

--- scripts/loader.js
+++ scripts/loader.js
@@ -3,7 +3,7 @@ const very = document.getElementsByClassName("navbar")[0].classList.contains("ve
 
 document.getElementsByClassName("navbar")[0].innerHTML = `
       <a href="/" class="button">Home</a>
-      <a href="/p" class="button">Proxy</a>
+      <a href="/m" class="button">Proxy</a>
       <a href="/more" class="button">Games</a>
       <a href="/help" class="button">Help</a>
       <a href="/chatroom" class="button">Chatroom</a>
