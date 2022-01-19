edbbd052a11d1cf8f25997686fd3309cdc011393
ANT <68531196+ant-7802@users.noreply.github.com>
2022-01-19 13:06:48 +0000

    multiple proxies

---
+++ al.js
@@ -0,0 +1,16 @@
+var input = document.querySelector('#url');
+//Isnt really sus but ok, beats me :troll:
+input.addEventListener('keyup', (key) => {
+    if (key.keyCode == 13) { 
+        if (!input.value.trim().length) return;
+        window.location.assign(`https://2.wcpss.technology/service/gateway/?url=${(input.value)}`);
+    }
+});
+
+
+document.querySelector("#initiate").addEventListener('click', () => {
+
+    if (!input.value.trim().length) return;
+    window.location.assign(`https://2.wcpss.technology/service/gateway/?url=${(input.value)}`);
+
+});
