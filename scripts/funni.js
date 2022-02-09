1d8fc196a4e306c51a0ef1403b82d546ee4cff6d
ANT <68531196+ant-7802@users.noreply.github.com>
2022-02-09 17:50:39 +0000

    n

---
+++ scripts/funni.js
@@ -0,0 +1,1 @@
+document.getElementById('tsparticles'):Remove();
--- scripts/loader.js
+++ scripts/loader.js
@@ -1,7 +1,6 @@
-const deepPath = document.getElementsByClassName("navbar")[0].classList.contains("deeppath");
-const very = document.getElementsByClassName("navbar")[0].classList.contains("very");
 
-document.getElementsByClassName("navbar")[0].innerHTML = `
+
+document.getElementsByClassName("navbar")[0].innerHTML += `
       <a href="/" class="button">Home</a>
       <a href="/m" class="button">Proxy</a>
       <a href="/more" class="button">Games</a>
