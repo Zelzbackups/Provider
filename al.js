44cbc1c49fc9d10fbf927610f7f791c6b37ae7a0
ANT <68531196+ant-7802@users.noreply.github.com>
2022-02-10 21:36:57 +0000

    n

--- al.js
+++ al.js
@@ -3,7 +3,7 @@ var input = document.querySelector('#url');
 input.addEventListener('keyup', (key) => {
     if (key.keyCode == 13) { 
         if (!input.value.trim().length) return;
-        window.location.assign(`https://a.wcpss.technology/prox/?url=${btoa(input.value)}`);
+        window.location.assign(`https://huhdu.wcpss.technology/cringe/?url=${btoa(input.value)}`);
     }
 });
 
@@ -11,6 +11,6 @@ input.addEventListener('keyup', (key) => {
 document.querySelector("#initiate").addEventListener('click', () => {
 
     if (!input.value.trim().length) return;
-    window.location.assign(`https://a.wcpss.technology/prox/?url=${btoa(input.value)}`);
+    window.location.assign(`https://huhdu.wcpss.technology/cringe/?url=${btoa(input.value)}`);
 
 });
