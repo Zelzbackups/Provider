0e92e14ee61023255675c784a38d93f1ca92bb4e
ant-7802 <anttubemc@googlemail.com>
2022-09-24 07:24:22 +0000

    added new providercord

--- scripts/loader.js
+++ scripts/loader.js
@@ -5,6 +5,8 @@ document.getElementsByClassName("navbar")[0].innerHTML += `
 <a href="/more" class="button">Games</a>
 <a href="/help" class="button">Help</a>
 <a href="https://forums.wcpss.technology" class="button">Forums</a>
+<a href="https://fc.wcpss.technology/login" class="button">ProviderCord</a>
+
  <script src="/scripts/image.js"></script>
 <script src="/scripts/name.js"></script>
 <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
