1d09cae2f9792cccf6db365849f994310aa5ad1a
ant-7802 <anttubemc@googlemail.com>
2023-01-11 15:25:28 +0000

    if I had sex with your mom pussy :((

--- scripts/loader.js
+++ scripts/loader.js
@@ -4,8 +4,9 @@ document.getElementsByClassName("navbar")[0].innerHTML += `
 <a href="/" class="button">Home</a>
 <a href="/more" class="button">Games</a>
 <a href="/help" class="button">Help</a>
+<a href="/shouts" class="button">Shoutouts</a>
 <a href="/qr" class="button">Discord</a>
-
+<a style="float: right;" href="/qr" class="button">⚙️</a>
  <script src="/scripts/image.js"></script>
 <script src="/scripts/name.js"></script>
 <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
