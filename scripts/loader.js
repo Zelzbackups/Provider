91147002735ca3add443f120339ba3c747380292
ant-7802 <anttubemc@googlemail.com>
2023-05-11 21:08:17 +0000

    fard

--- scripts/loader.js
+++ scripts/loader.js
@@ -5,6 +5,7 @@ document.getElementsByClassName("navbar")[0].innerHTML += `
 <a href="/more" class="button">Games</a>
 <a href="/help" class="button">Help</a>
 <a href="/shouts" class="button">Shoutouts</a>
+<a href="https://discord.gg/EjgWaaEDfQ" class="button">Discord</a>
 <a style="float: right;" href="/settings" class="button">⚙️</a>
  <script src="/scripts/image.js"></script>
 <script src="/scripts/name.js"></script>
